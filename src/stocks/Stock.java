b9b8dbeee027ffb9f4b0938b6f1bc9a28ffcc1cc
omerpessach <omerpessach7@gmail.com>
2021-03-04 16:56:54 +0200

    JDK 1.8 support

--- src/stocks/Stock.java
+++ src/stocks/Stock.java
@@ -25,7 +25,7 @@ public class Stock {
     }
 
     public void setSymbol(final String symbol) {
-        if (!symbol.isBlank()) {
+        if (!symbol.isEmpty()) {
             this.symbol = symbol;
         }
     }
@@ -35,7 +35,7 @@ public class Stock {
     }
 
     public void setCompanyName(final String companyName) {
-        if (!companyName.isBlank()) {
+        if (!companyName.isEmpty()) {
             this.companyName = companyName;
         }
     }
